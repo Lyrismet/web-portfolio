5e7971c907a08d81bab169ea266fef4da96a52fc
Lyrismet <ana0203tir@gmail.com>
2023-06-18 17:42:30 +0700

    refactor: cleaned

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.scss'
+import { Fira_Code} from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Fira_Code({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <header className="header">header</header>
+      {children}
+      <footer>footer</footer>
+      </body>
     </html>
   )
 }
