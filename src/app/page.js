80522ff0c5bda07c7fe05a1adbebbe25404054a4
Lyrismet <ana0203tir@gmail.com>
2023-06-18 17:41:07 +0700

    feat: added project component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-
-import styles from './page.module.css'
+import styles from './page.module.scss'
+import AllProjects from "@/components/project/allProjects";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+        <AllProjects/>
     </main>
   )
 }
