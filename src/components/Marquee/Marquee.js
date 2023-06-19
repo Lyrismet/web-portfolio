b02a4209d46aa6bdb1767333d97d2dd632cbd669
Lyrismet <ana0203tir@gmail.com>
2023-06-19 23:06:41 +0700

    feat: added layout modules

---
+++ src/components/Marquee/Marquee.js
@@ -0,0 +1,13 @@
+import './Marquee.scss'
+const Marquee = () => {
+    return (
+        <div className="marquee">
+            <marquee behavior="" direction="left">
+                <p>react javascript html5 css3 typescript scss next.js git semantic adaptive module.css webpack api prettier npm</p>
+                <p>react javascript html5 css3 typescript scss next.js git semantic adaptive module.css webpack api prettier npm</p>
+            </marquee>
+        </div>
+    );
+};
+
+export default Marquee;
