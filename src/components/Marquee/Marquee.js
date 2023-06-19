4da5510c06b8ca349879a2d290a3547b5ee8ff98
Lyrismet <ana0203tir@gmail.com>
2023-06-19 23:55:00 +0700

    feat: added running string

--- src/components/Marquee/Marquee.js
+++ src/components/Marquee/Marquee.js
@@ -2,7 +2,7 @@ import './Marquee.scss'
 const Marquee = () => {
     return (
         <div className="marquee">
-            <marquee behavior="" direction="left">
+            <marquee behavior="" direction="right">
                 <p>react javascript html5 css3 typescript scss next.js git semantic adaptive module.css webpack api prettier npm</p>
                 <p>react javascript html5 css3 typescript scss next.js git semantic adaptive module.css webpack api prettier npm</p>
             </marquee>
