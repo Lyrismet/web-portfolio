0e484b0fa4a1ce2bc47d468e61d984ee66de2923
Lyrismet <ana0203tir@gmail.com>
2023-06-19 21:22:36 +0700

    feat: added test modules

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import styles from './page.module.scss'
 import AllProjects from "@/components/project/allProjects";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+        <Navbar/>
         <AllProjects/>
     </main>
   )
