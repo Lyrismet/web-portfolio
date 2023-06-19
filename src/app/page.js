96a38f6ca1f846b63d6b99833c8f95d4ddedefa9
Lyrismet <ana0203tir@gmail.com>
2023-06-19 23:05:28 +0700

    feat: added layout modules

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import styles from './page.module.scss'
 import AllProjects from "@/components/project/allProjects";
-import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <Navbar/>
-        <AllProjects/>
+        {/*<AllProjects/>*/}
     </main>
   )
 }
