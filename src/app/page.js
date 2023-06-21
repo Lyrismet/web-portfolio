8d526777aabf746dbeb679c30c6d1094817f41e9
Lyrismet <ana0203tir@gmail.com>
2023-06-21 13:30:44 +0700

    feat: added main left fixed element

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import AllProjects from "@/components/project/allProjects";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.mainLeftFixed}></div>
         {/*<AllProjects/>*/}
     </main>
   )
