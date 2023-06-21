87762e46b00b8f6f13ef0e458893ba1f1500296f
Lyrismet <ana0203tir@gmail.com>
2023-06-21 21:04:33 +0700

    feat: added main canvas element

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.mainLeftFixed}></div>
+        <div className={styles.mainCanvas}></div>
         {/*<AllProjects/>*/}
     </main>
   )
