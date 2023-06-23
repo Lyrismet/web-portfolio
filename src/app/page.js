087286b3992d1f4b47f7662ebfcad4239ebeb148
Lyrismet <ana0203tir@gmail.com>
2023-06-23 10:53:27 +0700

    feat: added main canvas scroll

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,39 @@
+'use client'
 import styles from './page.module.scss'
+import {useEffect, useRef} from "react";
+import ScrollBooster from "scrollbooster";
 import AllProjects from "@/components/project/allProjects";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.mainLeftFixed}></div>
-        <div className={styles.mainCanvas}></div>
-        {/*<AllProjects/>*/}
-    </main>
-  )
+    const mainCanvasRef = useRef(null);
+    useEffect(
+        () => {
+            const viewport = mainCanvasRef.current;
+            const content = mainCanvasRef.current.querySelector('div')
+
+            const scrollBooster = new ScrollBooster({
+                viewport,
+                content,
+                scrollMode: 'native',
+                direction: 'horizontal'
+            });
+
+            return () => {
+                scrollBooster.destroy();
+            };
+        }, []
+    )
+
+    return (
+        <main className={styles.main}>
+            <div className={styles.mainLeftFixed}></div>
+            <div className={styles.mainCanvas} ref={mainCanvasRef}>
+                <div>
+
+                </div>
+
+            </div>
+            {/*<AllProjects/>*/}
+        </main>
+    )
 }
