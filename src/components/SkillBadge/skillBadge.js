c0e0cabb360b99a1a4a82a623d65f1500fc5e891
Lyrismet <ana0203tir@gmail.com>
2023-06-27 22:31:55 +0700

    feat: added full badges

--- src/components/SkillBadge/skillBadge.js
+++ src/components/SkillBadge/skillBadge.js
@@ -1,10 +1,9 @@
 import styles from './skillBadge.module.scss'
 
 
-const SkillBadge = ({ icon: Icon, text, color}) => {
+const SkillBadge = ({ text, color }) => {
     return (
-        <div style={{ borderColor: color}} className={styles.budge}>
-            <Icon style={{color: color}} className={styles.icon}/>
+        <div style={{ borderColor: color}} className={styles.badge}>
             <span style={{color: color}} className={styles.text}>{text}</span>
         </div>
     );
