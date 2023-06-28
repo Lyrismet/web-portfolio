2c945b74cc13338c4f5e3204fcc2638b69bddfff
Lyrismet <ana0203tir@gmail.com>
2023-06-29 02:13:59 +0700

    feat: added pre-full component

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,39 +1,75 @@
-import styles from './Project.module.scss'
+import React, { useState } from "react";
+import styles from "./Project.module.scss";
 import Image from "next/image";
-import {FiPaperclip} from "react-icons/fi";
+import { FiPaperclip } from "react-icons/fi";
 import SkillBadge from "@/components/SkillBadge/skillBadge";
 import projectsData from "@/data/projects.json";
+import {RiArrowGoBackLine} from "react-icons/ri";
 
-const Project = ({image, title, description, link, story, badges}) => {
+const Project = ({ image, title, description, link, story, badges }) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleHover = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+    };
+
+    const handleGoBack = () => {
+        setIsClicked(false);
+    };
+
     return (
         <div className={styles.project}>
-            <div className={styles.frontCard}>
+            <div
+                className={styles.frontCard}
+            >
                 <div className={styles.holoBackground}>
-                    <Image style={{borderRadius: "10px"}} src={image} alt={title} width={400}
-                           height={280}/>
+                    <Image
+                        style={{ borderRadius: "10px" }}
+                        src={image}
+                        alt={title}
+                        width={400}
+                        height={280}
+                    />
                 </div>
                 <div className={styles.wrapperName}>
                     <h2 className={styles.title}>{title}</h2>
-                    <button className={styles.buttonAttachments}>
-                        <FiPaperclip/>
+                    <button
+                        className={styles.buttonAttachments}
+                        onMouseEnter={handleHover}
+                        onMouseLeave={handleMouseLeave}
+                        onClick={handleClick}
+                    >
+                        <FiPaperclip />
                     </button>
                 </div>
                 <div className={styles.wrapperDesc}>
                     <p className={styles.description}>{description}</p>
-                    <a className={styles.link} href={link} target="_blank">🔗<span>Link to GitHub</span></a>
+                    <a className={styles.link} href={link} target="_blank">
+                        🔗<span>Link to GitHub</span>
+                    </a>
                     <div className={styles.badges}>
                         {badges.map((badge, index) => (
-                            <SkillBadge key={index} text={badge.text} color={badge.color}/>
+                            <SkillBadge key={index} text={badge.text} color={badge.color} />
                         ))}
                     </div>
                 </div>
             </div>
-            <div className={styles.backCard}>
-                <p className={styles.story}>
-                    {story}
-                </p>
+            <div
+                className={`${styles.backCard} ${isHovered ? styles.hoveredBackCard : ""} ${isClicked ? styles.clickedBackCard : ""}`}>
+                <p className={styles.story}>{story}</p>
+                <button className={styles.buttonAttachments} onClick={handleGoBack}>
+                    <RiArrowGoBackLine />
+                </button>
             </div>
-
         </div>
     );
 };
