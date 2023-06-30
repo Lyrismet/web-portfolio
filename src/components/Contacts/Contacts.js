b552f6c1991d0b7e2bdfdc0e75a710a8e9e874b4
Lyrismet <ana0203tir@gmail.com>
2023-07-01 01:42:32 +0700

    feat: created contacts component

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,14 @@
+import styles from './Contacts.module.scss'
+import {RiBehanceLine, RiGithubLine, RiMailLine, RiTelegramLine} from "react-icons/ri";
+
+export default function Contacts() {
+    return (
+        <div className={styles.contacts}>
+            <h1>Contact me:</h1>
+            <a href="mailto:ana0203tir@example.com"> O2h6n@example.com<RiMailLine/> </a>
+            <a href="https://github.com/Lyrismet">Github<RiGithubLine/> </a>
+            <a href="https://t.me/Lyrismet">Telegram<RiTelegramLine/> </a>
+            <a href="https://www.behance.net/lyrismet">Behance <RiBehanceLine/> </a>
+        </div>
+    )
+}
