f4fbda85fec188f77b19ec8553639762fb3b6e68
lyrismet <ana0203tir@gmail.com>
2023-07-16 22:21:13 +0300

    refactor: fixed text bug

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import styles from './About.module.scss'
 export default function About() {
     return (
         <div className={styles.about}>
-            <div id="about" className={styles.aboutInfo}>
+            <div className={styles.aboutInfo}>
                 <p className={styles.greetings}>✍🏻 Hi, I’m</p>
                 <h2 className={styles.title}>anastasia</h2>
                 <div className={styles.subtitleText}>frontend-developer</div>
@@ -14,8 +14,7 @@ export default function About() {
                     in which I
                     think through the functionality and design of an application.
                     I <span>value product development</span> in a team
-                    of experienced developers. In my spare time I like to study psychology, which allows me to have more
-                    <span>harmonious</span> relationships with my colleagues and increase my <span>productivity</span>
+                    of experienced developers. In my spare time I like to study psychology, which allows me to have more <span>harmonious</span> relationships with my colleagues and increase my <span>productivity</span>
                     .
                 </p>
             </div>
