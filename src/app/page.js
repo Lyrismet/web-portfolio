8e27995a3d6ce5e8b317bdf0d5b70a1646309157
lyrismet <ana0203tir@gmail.com>
2023-07-16 22:14:25 +0300

    feat: type of scrolling changed for more lightweight

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 import styles from './page.module.scss'
-import {useEffect, useRef} from "react";
-import ScrollBooster from "scrollbooster";
+import { useEffect, useRef } from "react";
+import { Element, scroller } from 'react-scroll';
 import Welcome from "@/components/Welcome/Welcome";
 import About from "@/components/About/About";
 import AllProjects from "@/components/Projects/allProjects";
@@ -10,40 +10,43 @@ import Contacts from "@/components/Contacts/Contacts";
 
 export default function Home() {
     const mainCanvasRef = useRef(null);
-    useEffect(
-        () => {
-            const viewport = mainCanvasRef.current;
-            const content = mainCanvasRef.current.querySelector('div')
 
-            const scrollBooster = new ScrollBooster({
-                viewport,
-                content,
-                scrollMode: 'native',
-                direction: 'horizontal',
-                pointerEvents: true
-            });
+    useEffect(() => {
+        const content = mainCanvasRef.current.querySelector('div');
+        const handleScroll = (event) => {
+            const delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
+            mainCanvasRef.current.scrollLeft -= delta * 80; // Adjust scroll speed here
+            event.preventDefault();
+        };
 
-            return () => {
-                scrollBooster.destroy();
-            };
-        }, []
-    )
+        mainCanvasRef.current.addEventListener('mousewheel', handleScroll, false);
+        mainCanvasRef.current.addEventListener('DOMMouseScroll', handleScroll, false);
+
+        return () => {
+            mainCanvasRef.current.removeEventListener('mousewheel', handleScroll, false);
+            mainCanvasRef.current.removeEventListener('DOMMouseScroll', handleScroll, false);
+        };
+    }, []);
 
     return (
         <main className={styles.main}>
             <div className={styles.mainLeftFixed}></div>
-            <div className={styles.mainCanvas} ref={mainCanvasRef}>
+            <div className={styles.mainCanvas} ref={mainCanvasRef} id="mainCanvas">
                 <div className={styles.wrapper}>
-                    <Welcome/>
-                   {/* <MoveToSection text="About"/>*/}
-                    <About/>
-                    {/*<MoveToSection text="Works"/>*/}
-                    <AllProjects/>
-                    <Contacts/>
+                    <Element name="welcome" className="element">
+                        <Welcome />
+                    </Element>
+                    <Element name="about" className="element">
+                        <About />
+                    </Element>
+                    <Element name="projects" className="element">
+                        <AllProjects />
+                    </Element>
+                    <Element style={{display: "flex"}} name="contacts" className="element">
+                        <Contacts />
+                    </Element>
                 </div>
-
             </div>
-
         </main>
-    )
+    );
 }
