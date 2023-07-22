f12bb3fd2757a9d313ee6738c7b0b78cead9e2f0
lyrismet <ana0203tir@gmail.com>
2023-07-22 19:18:25 +0300

    refactor: fix mobile ver

--- src/components/Marquee/Marquee.js
+++ src/components/Marquee/Marquee.js
@@ -2,15 +2,15 @@ import './Marquee.scss'
 const Marquee = () => {
     return (
         <div className="marquee">
-            <marquee scrollamount="8" behavior="" direction="left">
+            <marquee className="safari-hide" scrollamount="8" behavior="" direction="left">
                 <p className="marquee__front-text">react javascript next.js typescript json api</p>
                 <p className="marquee__back-text">react javascript next.js typescript json api</p>
             </marquee>
-            <marquee scrollamount="8" behavior="" direction="right">
+            <marquee className="safari-hide" scrollamount="8" behavior="" direction="right">
                 <p className="marquee__front-text">html5 css3 scss git semantic adaptive module.css</p>
                 <p className="marquee__back-text stroke-blue">html5 css3 scss git semantic adaptive module.css</p>
             </marquee>
-            <marquee scrollamount="8" behavior="" direction="left">
+            <marquee className="safari-hide" scrollamount="8" behavior="" direction="left">
                 <p className="marquee__front-text">git webpack prettier npm turbopack</p>
                 <p className="marquee__back-text">git webpack prettier npm turbopack</p>
             </marquee>
