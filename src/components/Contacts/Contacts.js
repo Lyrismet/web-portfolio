543267384fcba2423f0e4f4b22c0a7e0ab115c32
Lyrismet <ana0203tir@gmail.com>
2023-12-20 11:03:19 +0300

    ref: added _blank

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -5,10 +5,10 @@ export default function Contacts() {
     return (
         <div className={styles.contacts}>
             <h1>Contact me:</h1>
-            <a href="mailto:ana0203tir@gmail.com"> ana0203tir@gmail.com<RiMailLine/> </a>
-            <a href="https://github.com/Lyrismet">Github<RiGithubLine/> </a>
-            <a href="https://t.me/Lyrismet">Telegram<RiTelegramLine/> </a>
-            <a href="https://www.behance.net/lyrismet">Behance <RiBehanceLine/> </a>
+            <a href="mailto:ana0203tir@gmail.com" target="_blank"> ana0203tir@gmail.com<RiMailLine/> </a>
+            <a href="https://github.com/Lyrismet" target="_blank">Github<RiGithubLine/> </a>
+            <a href="https://t.me/Lyrismet" target="_blank">Telegram<RiTelegramLine/> </a>
+            <a href="https://www.behance.net/lyrismet" target="_blank">Behance <RiBehanceLine/> </a>
         </div>
     )
 }
