611cd1669b95173daa70d96b12ffad8f4e0f52c4
Lyrismet <ana0203tir@gmail.com>
2023-12-20 11:03:42 +0300

    ref: added skills

--- src/components/Marquee/Marquee.js
+++ src/components/Marquee/Marquee.js
@@ -3,16 +3,16 @@ const Marquee = () => {
     return (
         <div className="marquee">
             <marquee className="safari-hide" scrollamount="8" behavior="" direction="left">
-                <p className="marquee__front-text">react javascript next.js typescript json api</p>
-                <p className="marquee__back-text">react javascript next.js typescript json api</p>
+                <p className="marquee__front-text">react svelte javascript next.js sveltekit typescript json rest api</p>
+                <p className="marquee__back-text">react svelte javascript next.js sveltekit typescript json rest api</p>
             </marquee>
             <marquee className="safari-hide" scrollamount="8" behavior="" direction="right">
                 <p className="marquee__front-text">html5 css3 scss git semantic adaptive module.css</p>
                 <p className="marquee__back-text stroke-blue">html5 css3 scss git semantic adaptive module.css</p>
             </marquee>
             <marquee className="safari-hide" scrollamount="8" behavior="" direction="left">
-                <p className="marquee__front-text">git webpack prettier npm turbopack</p>
-                <p className="marquee__back-text">git webpack prettier npm turbopack</p>
+                <p className="marquee__front-text">git vite webpack prettier npm turbopack</p>
+                <p className="marquee__back-text">git vite webpack prettier npm turbopack</p>
             </marquee>
             <div className="marquee-mobile">
                 <p className="marquee__front-text">react javascript next.js typescript json api html5 css3 scss git semantic
